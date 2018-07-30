9cea85513731c1d36a61304ceae70d98cc25c983
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-30 20:11:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num_given)
+  
+  if((num_given%3==0)&&(num_given%5==0))
+   puts "FizzBuzz"
+   elsif (num_given%3==0)
+   puts "Fizz"
+   elsif (num_given%5==0)
+   puts "Buzz"
+ else
+   return nil
+  
+end
